4b66fa1e3dcc6633a69c6876200835ae04ac9f42
Lakshay Manglani <lakshaymanglani2212@gmail.com>
2024-07-08 09:14:48 +0530

    feat(api): add API configuration file for auth and user profile endpoints

---
+++ src/app/apiConfig.ts
@@ -0,0 +1,7 @@
+const BASE_URL = 'https://auth-service-production-2236.up.railway.app/api/v1';
+
+export const API_URLS = {
+  REGISTER: `${BASE_URL}/auth/register`,
+  LOGIN: `${BASE_URL}/auth/login`,
+  PROFILE: `${BASE_URL}/users/self`,
+};
