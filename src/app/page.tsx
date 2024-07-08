8e7734a091bdcfa5d693b0e9332ab483f5ed0197
Lakshay Manglani <lakshaymanglani2212@gmail.com>
2024-07-08 09:34:41 +0530

    feat(profile): add Profile page with user data fetching
    - Implemented fetching user data from API with authentication token.
    - Introduced `styles.module.css` for profile information styling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,75 @@
-export default function Home() {
+'use client';
+
+import {useEffect, useState} from 'react';
+import styles from './styles.module.css';
+import {useRouter} from 'next/navigation';
+import {API_URLS} from './apiConfig';
+import GlassContainer from './components/GlassContainer/GlassContainer';
+
+interface UserData {
+  givenName: string;
+  familyName: string;
+  username: string;
+  email: string;
+}
+
+export default function Profile() {
+  const router = useRouter();
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch user profile data with authentication token
+    const fetchUserProfile = async () => {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        router.push('/login');
+        return;
+      }
+      try {
+        const response = await fetch(API_URLS.PROFILE, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch user profile');
+        }
+
+        const responseData = await response.json();
+        setUserData(responseData.data);
+        setLoading(false);
+      } catch (error: any) {
+        console.error('Error fetching user profile:', error.message);
+        router.push('/login');
+      }
+    };
+
+    fetchUserProfile();
+  }, [router]);
+
   return (
-    <main>
-      <h1>Home</h1>
-    </main>
+    <GlassContainer maxWidth="400px">
+      <h1>User Profile</h1>
+      {loading && (
+        <div>
+          <div className={styles.name}>
+            <p>Loading...</p>
+          </div>
+          <div className={styles.email}>
+            <p>Loading...</p>
+          </div>
+        </div>
+      )}
+      {userData && (
+        <div>
+          <div className={styles.name}>
+            {userData.givenName + ' ' + userData.familyName}
+          </div>
+          <div className={styles.email}>{userData.email}</div>
+        </div>
+      )}
+    </GlassContainer>
   );
 }
